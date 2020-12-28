ee59ba0e3dbdafe440c05cab304e5ad44297b3af
Alex Vitkov <alexvitkov98@gmail.com>
2020-12-28 05:15:29 +0200

    operator precedence table

--- common.h
+++ common.h
@@ -17,27 +17,8 @@
 
 enum TokenType : u8 {
 	TOK_NONE = 0,
-	TOK_ID  = 128,
-	TOK_NUM,
 
-	KW_U8,
-	KW_U16,
-	KW_U32,
-	KW_U64,
-	KW_I8,
-	KW_I16,
-	KW_I32,
-	KW_I64,
-	KW_BOOL,
-	KW_F32,
-	KW_F64,
-	KW_FN,
-	KW_LET,
-	KW_STRUCT,
-	KW_TRUE,
-	KW_FALSE,
-
-    OP_PLUSPLUS,
+    OP_PLUSPLUS = 128,
     OP_MINUSMINUS,
     OP_SHIFTLEFT,
     OP_SHIFTRIGHT,
@@ -57,6 +38,28 @@ enum TokenType : u8 {
     OP_BITANDASSIGN,
     OP_BITXORASSIGN,
     OP_BITORASSIGN,
+
+	KW_U8,
+	KW_U16,
+	KW_U32,
+	KW_U64,
+	KW_I8,
+	KW_I16,
+	KW_I32,
+	KW_I64,
+	KW_BOOL,
+	KW_F32,
+	KW_F64,
+	KW_FN,
+	KW_LET,
+	KW_STRUCT,
+	KW_TRUE,
+	KW_FALSE,
+
+	TOK_ID  = 128,
+	TOK_NUM,
+
+    // VOLATILE, if you reorder these you have to change the 'prec' table in parser.cpp
 };
 
 #endif // guard
